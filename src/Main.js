1801d966b374e207ba1a793ca60791c38eecf001
AlexFedorko <alexfedorko15@gmail.com>
2021-09-02 09:38:23 +0300

    git init

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+export default function Main(){
+
+
+    return(
+        <>
+            <h1>Hello in the example of context</h1>
+            <button> Show alert</button>
+
+        </>
+    )
+}
